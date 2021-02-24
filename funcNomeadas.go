6b2cdd019fbd9d07522e82f856e45e3526947d2a
Almeida <thialmeida.mar@gmail.com>
2021-02-24 18:53:48 -0300

    Funções de Retorno Nomeado
    Funções de retorno Nomeado e sua sintaxe.

---
+++ funcNomeadas.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func cauculosMatematicos(n1, n2 int)(soma, subtracao int) {
+    soma = n1 + n2
+    subtracao = n1 - n2
+    return
+}
+
+func main() {
+soma1, subtracao1 := cauculosMatematicos(30, 20)
+
+fmt.Printf("A soma é: %d \n", soma1)
+fmt.Printf("A subtração é: %d \n", subtracao1)
+}
+
+/*Funções com retorno nomeado, são funções onde o retorno já declara a variável no parâmetro aí invés de apenas declarar o tipo do retorno*/
